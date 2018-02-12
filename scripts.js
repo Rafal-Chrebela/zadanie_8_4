d0c6c6031c3880039721916dc55bf1bd13bc4039
Rafał Chrebela <rafal.chrebela@gmail.com>
2018-02-12 22:16:55 +0100

    Add scripts.js with comments

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+/* first test comment
+*/
+    var name = prompt('Enter your name');
+    // this should be added later alert('Hello, ' + name); 
+    console.log('Hello, ' + name);
+
+
+/* another comment which doesn't mean anything
+*/
